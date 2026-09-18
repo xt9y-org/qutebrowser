4da2ad66e8d167b7c3c7d1baf9fa1463e6ecf811
xt9y <felix@xt9y.de>
2026-09-19 01:32:15 +0200

    refactor: avoid protected workspace command access

--- qutebrowser/components/workspacecommands.py
+++ qutebrowser/components/workspacecommands.py
@@ -7,13 +7,13 @@
 from qutebrowser.api import cmdutils
 from qutebrowser.browser import workspace
 from qutebrowser.completion.models import urlmodel
+from qutebrowser.mainwindow import mainwindow
 from qutebrowser.misc import objects
 from qutebrowser.utils import objreg
 
 
 def _open_filesystem(
     *,
-    dispatcher,
     tabbed_browser,
     path,
     related,
@@ -41,18 +41,15 @@ def _open_filesystem(
     filesystem_path = path if path not in (None, "") else None
 
     if window or private:
-        if private:
-            is_private = True
-        else:
-            is_private = tabbed_browser.is_private
-        target = dispatcher._new_tabbed_browser(is_private)
-        new_tab = target.workspace_tabopen(
+        is_private = True if private else tabbed_browser.is_private
+        target_window = mainwindow.MainWindow(private=is_private)
+        new_tab = target_window.tabbed_browser.workspace_tabopen(
             workspace.ContentKind.FILESYSTEM,
             path=filesystem_path,
             background=False,
             related=related,
         )
-        target.window().show()
+        target_window.show()
         return new_tab
 
     return tabbed_browser.workspace_tabopen(
@@ -111,7 +108,12 @@ def _register_workspace_open() -> None:
             window=win_id,
             from_command=True,
         )
-        tabbed_browser = dispatcher._tabbed_browser
+        tabbed_browser = objreg.get(
+            "tabbed-browser",
+            scope="window",
+            window=win_id,
+            from_command=True,
+        )
 
         if content_kind is workspace.ContentKind.BROWSER:
             current = tabbed_browser.widget.currentWidget()
@@ -133,7 +135,6 @@ def _register_workspace_open() -> None:
 
         if content_kind is workspace.ContentKind.FILESYSTEM:
             return _open_filesystem(
-                dispatcher=dispatcher,
                 tabbed_browser=tabbed_browser,
                 path=url,
                 related=related,
