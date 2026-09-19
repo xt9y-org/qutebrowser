28a797298a1420ab02161b5d17faf5e0d5208298
xt9y <felix@xt9y.de>
2026-09-19 11:19:22 +0200

    test: cover browser workspace adapter

--- tests/unit/browser/test_workspace.py
+++ tests/unit/browser/test_workspace.py
@@ -46,6 +46,29 @@ def test_resolve_content_kind_rejects_conflicts(kwargs):
         workspace.resolve_content_kind(**kwargs)
 
 
+def test_browser_content_adapter_delegates_to_web_tab():
+    class FakeTab:
+        def __init__(self):
+            self.focused = False
+
+        def title(self):
+            return "Browser title"
+
+        def setFocus(self):
+            self.focused = True
+
+    tab = FakeTab()
+    adapter = workspace.BrowserContentAdapter(tab)
+
+    assert adapter.kind is workspace.ContentKind.BROWSER
+    assert adapter.widget is tab
+    assert adapter.title() == "Browser title"
+    assert adapter.session_state() is None
+
+    adapter.focus()
+    assert tab.focused
+
+
 def test_content_session_is_immutable():
     session = workspace.ContentSession(
         workspace.ContentKind.FILESYSTEM,
