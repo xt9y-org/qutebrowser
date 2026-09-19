1f0af626fba8ca12731dab93b02317c2a887d953
xt9y <felix@xt9y.de>
2026-09-19 11:06:37 +0200

    refactor: keep workspace abstraction parser independent

--- qutebrowser/browser/workspace.py
+++ qutebrowser/browser/workspace.py
@@ -6,7 +6,6 @@
 
 from __future__ import annotations
 
-import argparse
 import dataclasses
 import enum
 from typing import TYPE_CHECKING, Protocol
@@ -54,50 +53,6 @@ class WorkspaceContent(Protocol):
     def session_state(self) -> ContentSession | None:
         """Return restorable state, or None when the content is ephemeral."""
         ...
-
-
-_SELECTOR_DEST = "workspace_application"
-_SELECTOR_FLAGS = (
-    ("-at", "--application-terminal", ContentKind.TERMINAL),
-    ("-ab", "--application-browser", ContentKind.BROWSER),
-    ("-af", "--application-filesystem", ContentKind.FILESYSTEM),
-)
-
-
-class _ApplicationSelectorAction(argparse.Action):
-    """Store one application selector while rejecting conflicting selectors."""
-
-    def __init__(self, option_strings, dest, *, content_kind, **kwargs):
-        self._content_kind = content_kind
-        super().__init__(option_strings, dest, nargs=0, **kwargs)
-
-    def __call__(self, parser, namespace, _values, option_string=None):
-        previous = getattr(namespace, self.dest, None)
-        if previous is not None and previous is not self._content_kind:
-            parser.error("Only one of -at/-ab/-af can be given!")
-        setattr(namespace, self.dest, self._content_kind)
-
-
-def add_application_selector_arguments(parser: argparse.ArgumentParser) -> None:
-    """Add -at/-ab/-af selectors to a command parser."""
-    parser.set_defaults(**{_SELECTOR_DEST: None})
-    for short_flag, long_flag, content_kind in _SELECTOR_FLAGS:
-        parser.add_argument(
-            short_flag,
-            long_flag,
-            dest=_SELECTOR_DEST,
-            action=_ApplicationSelectorAction,
-            content_kind=content_kind,
-            help="Open {} workspace content.".format(content_kind.value),
-        )
-
-
-def selected_content_kind(namespace: argparse.Namespace) -> ContentKind:
-    """Return the application selected in a parsed command namespace."""
-    selected = getattr(namespace, _SELECTOR_DEST, None)
-    if selected is None:
-        return ContentKind.BROWSER
-    return selected
 
 
 def resolve_content_kind(
